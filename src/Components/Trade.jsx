329188392a19ffe3f021a1ffbe6621f5063bf3ef
MalvikaS1211 <malvikasrivastava.nadcab@gmail.com>
2025-10-06 14:37:25 +0530

    msg 25 to 10

--- src/Components/Trade.jsx
+++ src/Components/Trade.jsx
@@ -797,7 +797,7 @@ export default function Trade() {
                 </h2>
               </div>
               <div style={{ color: "black", fontSize: "14px" }}>
-                💰 An additional platform fee of 25 USDT will be applicable only
+                💰 An additional platform fee of 10 USDT will be applicable only
                 on the first package. ✅ This is a one-time fee to activate
                 smooth operations and ensure reliable services.
               </div>
